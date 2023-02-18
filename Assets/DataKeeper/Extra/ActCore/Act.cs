35ba0df18b642c7b14437fc022ca1cad14b48272
roman.shulevka@pettersonapps.com <micrarriors@gmail.com>
2023-02-19 00:38:13 +0200

    splited code of Coroutines and Enumerators

--- Assets/DataKeeper/Extra/ActCore/Act.cs
+++ Assets/DataKeeper/Extra/ActCore/Act.cs
@@ -11,15 +11,16 @@ namespace DataKeeper.Extra.ActCore
     {
         private static ActEngine _actEngine;
 
-        public static ActEngine Engine
+        private static ActEngine Engine
         {
             get
             {
-                Init();
+                if(!IsInitialized) Init();
                 return _actEngine;
             }
         }
 
+        public static bool IsInitialized { get; private set; } = false;
         public static UnityEvent OnApplicationQuitEvent => Engine.OnApplicationQuitEvent;
         public static UnityEvent<bool> OnApplicationFocusEvent => Engine.OnApplicationFocusEvent;
         public static UnityEvent<bool> OnApplicationPauseEvent => Engine.OnApplicationPauseEvent;
@@ -34,127 +35,30 @@ namespace DataKeeper.Extra.ActCore
         {
             if (_actEngine != null) return;
 
+            IsInitialized = true;
             GameObject gameObject = new GameObject("[ActEngine]");
             _actEngine = gameObject.AddComponent<ActEngine>();
             Object.DontDestroyOnLoad(gameObject);
         }
-
-        public static void Float(float from, float to, float duration, Action<float> value, Action onComplete = null)
-        {
-            Engine?.StartCoroutine(FloatTimer(from, to, duration, value, onComplete));
-        }
         
-        public static void Int(int from, int to, float duration, Action<int> value, Action onComplete = null)
-        {
-            Engine?.StartCoroutine(IntTimer(from, to, duration, value, onComplete));
-        }
+        public static Coroutine StartCoroutine(IEnumerator coroutine) => Engine.StartCoroutine(coroutine);
+        public static void StopCoroutine(Coroutine coroutine) { if (coroutine != null) Engine.StopCoroutine(coroutine); }
 
-        private static IEnumerator FloatTimer(float from, float to, float duration, Action<float> value, Action onComplete)
-        {
-            var time = 0f;
-            value?.Invoke(from);
-            
-            while (time <= duration)
-            {
-                yield return new WaitForEndOfFrame();
-                time += Time.deltaTime;
-                
-                value?.Invoke(Lerp.Float(from, to, time / duration));
-            }
-            
-            value?.Invoke(to);
-            onComplete?.Invoke();
-        }
-        
-        private static IEnumerator IntTimer(int from, int to, float duration, Action<int> value, Action onComplete)
-        {
-            var time = 0f;
-            value?.Invoke(from);
-            
-            while (time <= duration)
-            {
-                yield return new WaitForEndOfFrame();
-                time += Time.deltaTime;
-                
-                value?.Invoke(Lerp.Int(from, to, time / duration));
-            }
-            
-            value?.Invoke(to);
-            onComplete?.Invoke();
-        }
+        public static Coroutine OneSecondUpdate(Action callback) => StartCoroutine(ActEnumerator.OneSecondUpdate(callback));
+
+        public static void Float(float from, float to, float duration, Action<float> value, Action onComplete = null) => StartCoroutine(ActEnumerator.Float(from, to, duration, value, onComplete));
+        public static void One(float duration, Action<float> value, Action onComplete = null) => StartCoroutine(ActEnumerator.Float(0f, 1f, duration, value, onComplete));
+        public static void Int(int from, int to, float duration, Action<int> value, Action onComplete = null) => StartCoroutine(ActEnumerator.Int(from, to, duration, value, onComplete));
+
 
         /// <summary>
         /// If time less than 0 - wait 0 seconds. If time equals 0 - wait 1 frame. If time greater than 0 - wait in seconds.
         /// </summary>
         /// <param name="time">in seconds.</param>
         /// <param name="callback">callback on timeout.</param>
-        public static void DelayedCall(float time, Action callback)
-        {
-            StartCoroutine(WaitSeconds(time, callback));
-        }
-    
-        private static IEnumerator WaitSeconds(float time, Action callback)
-        {
-            if (time == 0)
-            {
-                yield return new WaitForEndOfFrame();
-            }
-            else if (time > 0)
-            {
-                yield return new WaitForSeconds(time);
-            }
-            callback?.Invoke();
-        }
-        
-        public static Coroutine StartCoroutine(IEnumerator coroutine)
-        {
-            return Engine?.StartCoroutine(coroutine);
-        }
-        
-        public static void StopCoroutine(Coroutine coroutine)
-        {
-            if (coroutine != null)
-            {
-                Engine?.StopCoroutine(coroutine);
-            }
-        }
-        
-        public static Coroutine OneSecondUpdate(Action callback)
-        {
-            return StartCoroutine(OneSecond(callback));
-        }
-    
-        private static IEnumerator OneSecond(Action callback)
-        {
-            callback?.Invoke();
-            
-            while (true)
-            {
-                yield return new WaitForSeconds(1);
-                callback?.Invoke();
-            }
-        }
-        
-        public static Coroutine WaitWhile(Func<bool> wait, Action callback)
-        {
-            return StartCoroutine(WaitWhileProcess(wait, callback));
-        }
-    
-        private static IEnumerator WaitWhileProcess(Func<bool> wait, Action callback)
-        {
-            yield return new WaitWhile(wait);
-            callback?.Invoke();
-        }
+        public static void DelayedCall(float time, Action callback) => StartCoroutine(ActEnumerator.WaitSeconds(time, callback));
 
-        public static Coroutine WaitUntil(Func<bool> wait, Action callback)
-        {
-            return StartCoroutine(WaitUntilProcess(wait, callback));
-        }
-    
-        private static IEnumerator WaitUntilProcess(Func<bool> wait, Action callback)
-        {
-            yield return new WaitUntil(wait);
-            callback?.Invoke();
-        }
+        public static Coroutine WaitWhile(Func<bool> wait, Action callback) => StartCoroutine(ActEnumerator.WaitWhile(wait, callback));
+        public static Coroutine WaitUntil(Func<bool> wait, Action callback) => StartCoroutine(ActEnumerator.WaitUntil(wait, callback));
     }
 }
---
+++ Assets/DataKeeper/Extra/ActCore/ActEnumerator.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+namespace DataKeeper.Extra.ActCore
+{
+    public static class ActEnumerator
+    {
+        public static IEnumerator WaitUntil(Func<bool> wait, Action callback)
+        {
+            yield return new WaitUntil(wait);
+            callback?.Invoke();
+        }
+    
+        public static IEnumerator WaitWhile(Func<bool> wait, Action callback)
+        {
+            yield return new WaitWhile(wait);
+            callback?.Invoke();
+        }
+    
+        public static IEnumerator OneSecondUpdate(Action callback)
+        {
+            callback?.Invoke();
+            
+            while (true)
+            {
+                callback?.Invoke();
+                yield return new WaitForSeconds(1);
+            }
+        }
+    
+        public static IEnumerator WaitSeconds(float time, Action callback)
+        {
+            if (time == 0)
+            {
+                yield return new WaitForEndOfFrame();
+            }
+            else if (time > 0)
+            {
+                yield return new WaitForSeconds(time);
+            }
+            callback?.Invoke();
+        }
+    
+        public static IEnumerator Int(int from, int to, float duration, Action<int> value, Action onComplete)
+        {
+            var time = 0f;
+            value?.Invoke(from);
+            
+            while (time <= duration)
+            {
+                yield return new WaitForEndOfFrame();
+                time += Time.deltaTime;
+                
+                value?.Invoke(Lerp.Int(from, to, time / duration));
+            }
+            
+            value?.Invoke(to);
+            onComplete?.Invoke();
+        }
+    
+        public static IEnumerator Float(float from, float to, float duration, Action<float> value, Action onComplete)
+        {
+            var time = 0f;
+            value?.Invoke(from);
+            
+            while (time <= duration)
+            {
+                yield return new WaitForEndOfFrame();
+                time += Time.deltaTime;
+                
+                value?.Invoke(Lerp.Float(from, to, time / duration));
+            }
+            
+            value?.Invoke(to);
+            onComplete?.Invoke();
+        }
+    }
+}
