a19ff9461ea90cc598ce294fb62ccd58a77c3e7e
roman.shulevka@pettersonapps.com <micrarriors@gmail.com>
2023-02-19 01:33:26 +0200

    added ease: linear, sin, cos

--- Assets/DataKeeper/Extra/ActCore/Lerp.cs
+++ Assets/DataKeeper/Extra/ActCore/Lerp.cs
@@ -1,15 +1,37 @@
-﻿namespace DataKeeper.Extra.ActCore
+﻿using UnityEngine;
+
+namespace DataKeeper.Extra.ActCore
 {
     public static class Lerp
     {
-        public static float Float(float a, float b, float t)
+        public static float Float(float from, float to, float value)
         {
-            return a + (b - a) * t;
+            return Mathf.Lerp(from, to, value);
         }
 
-        public static int Int(int a, int b, float t)
+        public static int Int(int from, int to, float value)
         {
-            return (int)(a + (b - a) * t);
+            return (int)Mathf.Lerp(from, to, value);
+        }
+        
+        public static float Remap(this float value, float fromMin, float fromMax, float toMin,  float toMax)
+        {
+            return Mathf.Lerp(toMin, toMax, Mathf.InverseLerp(fromMin, fromMax, value));
+        }
+    
+        public static float Map(this float value, float from, float to)
+        {
+            return Mathf.Lerp(from, to, value);
+        }
+        
+        public static int Remap(this int value, int fromMin, int fromMax, int toMin,  int toMax)
+        {
+            return (int)Mathf.Lerp(toMin, toMax, Mathf.InverseLerp(fromMin, fromMax, value));
+        }
+    
+        public static int Map(this int value, int from, int to)
+        {
+            return (int)Mathf.Lerp(from, to, value);
         }
     }
 }
