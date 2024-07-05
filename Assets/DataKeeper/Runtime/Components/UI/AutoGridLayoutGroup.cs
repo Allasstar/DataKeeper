b655647e6a1fe0d54d3a2d53ef90b809a0f0dfcd
roman.shulevka@pettersonapps.com <micrarriors@gmail.com>
2024-07-06 01:38:12 +0300

    Fixed AutoGridLayoutGroup

--- Assets/DataKeeper/Runtime/Components/UI/AutoGridLayoutGroup.cs
+++ Assets/DataKeeper/Runtime/Components/UI/AutoGridLayoutGroup.cs
@@ -1,49 +1,121 @@
+using DataKeeper.Attributes;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace DataKeeper.Components.UI
 {
     [AddComponentMenu("DataKeeper/UI/Auto Grid Layout Group")]
+    /// <summary>
+    /// Layout class to arrange child elements in a grid format.
+    /// </summary>
+    /// <remarks>
+    /// The GridLayoutGroup component is used to layout child layout elements in a uniform grid where all cells have the same size. The size and the spacing between cells is controlled by the GridLayoutGroup itself. The children have no influence on their sizes.
+    /// </remarks>
     public class AutoGridLayoutGroup : LayoutGroup
     {
+        /// <summary>
+        /// Which corner is the starting corner for the grid.
+        /// </summary>
         public enum Corner
         {
+            /// <summary>
+            /// Upper Left corner.
+            /// </summary>
             UpperLeft = 0,
+            /// <summary>
+            /// Upper Right corner.
+            /// </summary>
             UpperRight = 1,
+            /// <summary>
+            /// Lower Left corner.
+            /// </summary>
             LowerLeft = 2,
+            /// <summary>
+            /// Lower Right corner.
+            /// </summary>
             LowerRight = 3
         }
 
+        /// <summary>
+        /// The grid axis we are looking at.
+        /// </summary>
+        /// <remarks>
+        /// As the storage is a [][] we make access easier by passing a axis.
+        /// </remarks>
         public enum Axis
         {
+            /// <summary>
+            /// Horizontal axis
+            /// </summary>
             Horizontal = 0,
+            /// <summary>
+            /// Vertical axis.
+            /// </summary>
             Vertical = 1
         }
 
+        /// <summary>
+        /// Constraint type on either the number of columns or rows.
+        /// </summary>
         public enum Constraint
         {
-            FixedColumnCount = 1,
-            FixedRowCount = 2
+            /// <summary>
+            /// Constrain the number of columns to a specified number.
+            /// </summary>
+            FixedColumnCount = 0,
+            /// <summary>
+            /// Constraint the number of rows to a specified number.
+            /// </summary>
+            FixedRowCount = 1
         }
 
         [SerializeField] protected Corner m_StartCorner = Corner.UpperLeft;
-        [SerializeField] protected Vector2 m_Spacing = Vector2.zero;
-        [SerializeField] protected float m_ChildAspectRatio = 1f;
-        [SerializeField] protected Constraint m_Constraint = Constraint.FixedColumnCount;
-        [SerializeField] protected int m_ConstraintCount = 2;
 
+        /// <summary>
+        /// Which corner should the first cell be placed in?
+        /// </summary>
         public Corner startCorner { get { return m_StartCorner; } set { SetProperty(ref m_StartCorner, value); } }
 
-        protected Axis m_StartAxis = Axis.Horizontal;
+        [SerializeField] protected Axis m_StartAxis = Axis.Horizontal;
+
+        /// <summary>
+        /// Which axis should cells be placed along first
+        /// </summary>
+        /// <remarks>
+        /// When startAxis is set to horizontal, an entire row will be filled out before proceeding to the next row. When set to vertical, an entire column will be filled out before proceeding to the next column.
+        /// </remarks>
         public Axis startAxis { get { return m_StartAxis; } set { SetProperty(ref m_StartAxis, value); } }
 
+        [SerializeField] protected RectTransform m_gridContainer;
+
+        [SerializeField, ReadOnlyInspector] protected Vector2 m_CellSize = new Vector2(100, 100);
+
+        [SerializeField] protected Vector2 m_Spacing = Vector2.zero;
+
+        /// <summary>
+        /// The spacing to use between layout elements in the grid on both axises.
+        /// </summary>
         public Vector2 spacing { get { return m_Spacing; } set { SetProperty(ref m_Spacing, value); } }
 
+        [SerializeField] protected Constraint m_Constraint = Constraint.FixedColumnCount;
+
+        /// <summary>
+        /// Which constraint to use for the GridLayoutGroup.
+        /// </summary>
+        /// <remarks>
+        /// Specifying a constraint can make the GridLayoutGroup work better in conjunction with a [[ContentSizeFitter]] component. When GridLayoutGroup is used on a RectTransform with a manually specified size, there's no need to specify a constraint.
+        /// </remarks>
         public Constraint constraint { get { return m_Constraint; } set { SetProperty(ref m_Constraint, value); } }
 
+        [SerializeField] protected int m_ConstraintCount = 2;
+
+        /// <summary>
+        /// How many cells there should be along the constrained axis.
+        /// </summary>
         public int constraintCount { get { return m_ConstraintCount; } set { SetProperty(ref m_ConstraintCount, Mathf.Max(1, value)); } }
 
-        public float childAspectRatio { get { return m_ChildAspectRatio; } set { SetProperty(ref m_ChildAspectRatio, value); } }
+        [SerializeField] protected float m_AspectRatio = 1;
+        public float aspectRatio { get { return m_AspectRatio; } set { SetProperty(ref m_AspectRatio, value); } }
 
         protected AutoGridLayoutGroup()
         {}
@@ -54,56 +126,103 @@ namespace DataKeeper.Components.UI
             base.OnValidate();
             constraintCount = constraintCount;
         }
-        #endif
 
+        #endif
+        
+        /// <summary>
+        /// Called by the layout system to calculate the horizontal layout size.
+        /// Also see ILayoutElement
+        /// </summary>
         public override void CalculateLayoutInputHorizontal()
         {
             base.CalculateLayoutInputHorizontal();
 
-            int minColumns = 1;
-            int preferredColumns = 1;
+            if (m_gridContainer == null)
+            {
+                m_CellSize.x = 100f;
+                m_CellSize.y = 100f;
+            }
+            else
+            {
+                if (constraint == Constraint.FixedColumnCount)
+                {
+                    m_CellSize.x = m_gridContainer.rect.width - (spacing.x * (constraintCount - 1) + padding.horizontal);
+                    m_CellSize.x /= constraintCount;
+                    m_CellSize.y = m_CellSize.x / aspectRatio;
+                }
+                else if (constraint == Constraint.FixedRowCount)
+                {
+                    m_CellSize.y = m_gridContainer.rect.height - (spacing.y * (constraintCount - 1) + padding.vertical);
+                    m_CellSize.y /= constraintCount;
+                    
+                    m_CellSize.x = m_CellSize.y * aspectRatio;
+                }
 
+                m_CellSize.x = Mathf.Max(0, m_CellSize.x);
+                m_CellSize.y = Mathf.Max(0, m_CellSize.y);
+            }
+
+            int minColumns = 0;
+            int preferredColumns = 0;
             if (m_Constraint == Constraint.FixedColumnCount)
             {
-                startAxis = Axis.Horizontal;
                 minColumns = preferredColumns = m_ConstraintCount;
             }
             else if (m_Constraint == Constraint.FixedRowCount)
             {
-                startAxis = Axis.Vertical;
-                minColumns = preferredColumns = Mathf.CeilToInt(rectChildren.Count / (float)m_ConstraintCount);
+                minColumns = preferredColumns = Mathf.CeilToInt(rectChildren.Count / (float)m_ConstraintCount - 0.001f);
+            }
+            else
+            {
+                minColumns = 1;
+                preferredColumns = Mathf.CeilToInt(Mathf.Sqrt(rectChildren.Count));
             }
 
             SetLayoutInputForAxis(
-                padding.horizontal + (rectTransform.rect.width / minColumns + spacing.x) * minColumns - spacing.x,
-                padding.horizontal + (rectTransform.rect.width / preferredColumns + spacing.x) * preferredColumns - spacing.x,
+                padding.horizontal + (m_CellSize.x + spacing.x) * minColumns - spacing.x,
+                padding.horizontal + (m_CellSize.x + spacing.x) * preferredColumns - spacing.x,
                 -1, 0);
         }
 
+        /// <summary>
+        /// Called by the layout system to calculate the vertical layout size.
+        /// Also see ILayoutElement
+        /// </summary>
         public override void CalculateLayoutInputVertical()
         {
-            int minRows = 1;
-            float cellWidth = (rectTransform.rect.width - padding.horizontal - spacing.x * (m_ConstraintCount - 1)) / m_ConstraintCount;
-            float cellHeight = cellWidth / m_ChildAspectRatio;
-
+            int minRows = 0;
             if (m_Constraint == Constraint.FixedColumnCount)
             {
-                minRows = Mathf.CeilToInt(rectChildren.Count / (float)m_ConstraintCount);
+                minRows = Mathf.CeilToInt(rectChildren.Count / (float)m_ConstraintCount - 0.001f);
             }
             else if (m_Constraint == Constraint.FixedRowCount)
             {
                 minRows = m_ConstraintCount;
             }
+            else
+            {
+                float width = rectTransform.rect.width;
+                int cellCountX = Mathf.Max(1, Mathf.FloorToInt((width - padding.horizontal + spacing.x + 0.001f) / (m_CellSize.x + spacing.x)));
+                minRows = Mathf.CeilToInt(rectChildren.Count / (float)cellCountX);
+            }
 
-            float minSpace = padding.vertical + (cellHeight + spacing.y) * minRows - spacing.y;
+            float minSpace = padding.vertical + (m_CellSize.y + spacing.y) * minRows - spacing.y;
             SetLayoutInputForAxis(minSpace, minSpace, -1, 1);
         }
 
+        /// <summary>
+        /// Called by the layout system
+        /// Also see ILayoutElement
+        /// </summary>
         public override void SetLayoutHorizontal()
         {
             SetCellsAlongAxis(0);
         }
 
+        /// <summary>
+        /// Called by the layout system
+        /// Also see ILayoutElement
+        /// </summary>
         public override void SetLayoutVertical()
         {
             SetCellsAlongAxis(1);
@@ -111,33 +230,62 @@ namespace DataKeeper.Components.UI
 
         private void SetCellsAlongAxis(int axis)
         {
+            // Normally a Layout Controller should only set horizontal values when invoked for the horizontal axis
+            // and only vertical values when invoked for the vertical axis.
+            // However, in this case we set both the horizontal and vertical position when invoked for the vertical axis.
+            // Since we only set the horizontal position and not the size, it shouldn't affect children's layout,
+            // and thus shouldn't break the rule that all horizontal layout must be calculated before all vertical layout.
+            var rectChildrenCount = rectChildren.Count;
+            if (axis == 0)
+            {
+                // Only set the sizes when invoked for horizontal axis, not the positions.
+
+                for (int i = 0; i < rectChildrenCount; i++)
+                {
+                    RectTransform rect = rectChildren[i];
+
+                    m_Tracker.Add(this, rect,
+                        DrivenTransformProperties.Anchors |
+                        DrivenTransformProperties.AnchoredPosition |
+                        DrivenTransformProperties.SizeDelta);
+
+                    rect.anchorMin = Vector2.up;
+                    rect.anchorMax = Vector2.up;
+                    rect.sizeDelta = m_CellSize;
+                }
+                return;
+            }
+
             float width = rectTransform.rect.size.x;
             float height = rectTransform.rect.size.y;
 
             int cellCountX = 1;
             int cellCountY = 1;
-
             if (m_Constraint == Constraint.FixedColumnCount)
             {
                 cellCountX = m_ConstraintCount;
-                cellCountY = Mathf.CeilToInt(rectChildren.Count / (float)cellCountX);
+
+                if (rectChildrenCount > cellCountX)
+                    cellCountY = rectChildrenCount / cellCountX + (rectChildrenCount % cellCountX > 0 ? 1 : 0);
             }
             else if (m_Constraint == Constraint.FixedRowCount)
             {
                 cellCountY = m_ConstraintCount;
-                cellCountX = Mathf.CeilToInt(rectChildren.Count / (float)cellCountY);
-            }
 
-            Vector2 cellSize = Vector2.zero;
-            if (m_Constraint == Constraint.FixedColumnCount)
-            {
-                cellSize.x = (width - spacing.x * (cellCountX - 1) - padding.horizontal) / cellCountX;
-                cellSize.y = cellSize.x / m_ChildAspectRatio;
+                if (rectChildrenCount > cellCountY)
+                    cellCountX = rectChildrenCount / cellCountY + (rectChildrenCount % cellCountY > 0 ? 1 : 0);
             }
-            else // FixedRowCount
+            else
             {
-                cellSize.y = (height - spacing.y * (cellCountY - 1) - padding.vertical) / cellCountY;
-                cellSize.x = cellSize.y * m_ChildAspectRatio;
+                if (m_CellSize.x + spacing.x <= 0)
+                    cellCountX = int.MaxValue;
+                else
+                    cellCountX = Mathf.Max(1, Mathf.FloorToInt((width - padding.horizontal + spacing.x + 0.001f) / (m_CellSize.x + spacing.x)));
+
+                if (m_CellSize.y + spacing.y <= 0)
+                    cellCountY = int.MaxValue;
+                else
+                    cellCountY = Mathf.Max(1, Mathf.FloorToInt((height - padding.vertical + spacing.y + 0.001f) / (m_CellSize.y + spacing.y)));
             }
 
             int cornerX = (int)startCorner % 2;
@@ -147,26 +295,26 @@ namespace DataKeeper.Components.UI
             if (startAxis == Axis.Horizontal)
             {
                 cellsPerMainAxis = cellCountX;
-                actualCellCountX = Mathf.Clamp(cellCountX, 1, rectChildren.Count);
-                actualCellCountY = Mathf.Clamp(cellCountY, 1, Mathf.CeilToInt(rectChildren.Count / (float)cellsPerMainAxis));
+                actualCellCountX = Mathf.Clamp(cellCountX, 1, rectChildrenCount);
+                actualCellCountY = Mathf.Clamp(cellCountY, 1, Mathf.CeilToInt(rectChildrenCount / (float)cellsPerMainAxis));
             }
             else
             {
                 cellsPerMainAxis = cellCountY;
-                actualCellCountY = Mathf.Clamp(cellCountY, 1, rectChildren.Count);
-                actualCellCountX = Mathf.Clamp(cellCountX, 1, Mathf.CeilToInt(rectChildren.Count / (float)cellsPerMainAxis));
+                actualCellCountY = Mathf.Clamp(cellCountY, 1, rectChildrenCount);
+                actualCellCountX = Mathf.Clamp(cellCountX, 1, Mathf.CeilToInt(rectChildrenCount / (float)cellsPerMainAxis));
             }
 
             Vector2 requiredSpace = new Vector2(
-                actualCellCountX * cellSize.x + (actualCellCountX - 1) * spacing.x,
-                actualCellCountY * cellSize.y + (actualCellCountY - 1) * spacing.y
+                actualCellCountX * m_CellSize.x + (actualCellCountX - 1) * spacing.x,
+                actualCellCountY * m_CellSize.y + (actualCellCountY - 1) * spacing.y
             );
             Vector2 startOffset = new Vector2(
                 GetStartOffset(0, requiredSpace.x),
                 GetStartOffset(1, requiredSpace.y)
             );
 
-            for (int i = 0; i < rectChildren.Count; i++)
+            for (int i = 0; i < rectChildrenCount; i++)
             {
                 int positionX;
                 int positionY;
@@ -186,8 +334,8 @@ namespace DataKeeper.Components.UI
                 if (cornerY == 1)
                     positionY = actualCellCountY - 1 - positionY;
 
-                SetChildAlongAxis(rectChildren[i], 0, startOffset.x + (cellSize[0] + spacing[0]) * positionX, cellSize[0]);
-                SetChildAlongAxis(rectChildren[i], 1, startOffset.y + (cellSize[1] + spacing[1]) * positionY, cellSize[1]);
+                SetChildAlongAxis(rectChildren[i], 0, startOffset.x + (m_CellSize[0] + spacing[0]) * positionX, m_CellSize[0]);
+                SetChildAlongAxis(rectChildren[i], 1, startOffset.y + (m_CellSize[1] + spacing[1]) * positionY, m_CellSize[1]);
             }
         }
     }
