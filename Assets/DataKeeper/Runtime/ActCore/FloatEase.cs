b42421b913c229ee1ad05bc25c54b974744a54db
roman.shulevka@pettersonapps.com <micrarriors@gmail.com>
2024-07-19 23:45:09 +0300

    Update FloatEase.cs

--- Assets/DataKeeper/Runtime/ActCore/FloatEase.cs
+++ Assets/DataKeeper/Runtime/ActCore/FloatEase.cs
@@ -1,27 +1,30 @@
+using System;
+
 namespace DataKeeper.ActCore
 {
-    public struct FloatEase
+    public readonly struct FloatEase : IEquatable<FloatEase>, IComparable<FloatEase>
     {
         private readonly float _value;
 
-        private FloatEase(float value)
+        public FloatEase(float value)
         {
             _value = value;
         }
 
-        public static implicit operator float(FloatEase target)
-        {
-            return target._value;
-        }
+        public static implicit operator float(FloatEase target) => target._value;
+        public static implicit operator FloatEase(float target) => new FloatEase(target);
 
-        public static implicit operator FloatEase(float target)
-        {
-            return new FloatEase(target);
-        }
+        public static FloatEase operator +(FloatEase a, FloatEase b) => new FloatEase(a._value + b._value);
+        public static FloatEase operator -(FloatEase a, FloatEase b) => new FloatEase(a._value - b._value);
+        public static FloatEase operator *(FloatEase a, FloatEase b) => new FloatEase(a._value * b._value);
+        public static FloatEase operator /(FloatEase a, FloatEase b) => new FloatEase(a._value / b._value);
 
-        public override string ToString()
-        {
-            return _value.ToString();
-        }
+        public bool Equals(FloatEase other) => _value.Equals(other._value);
+        public override bool Equals(object obj) => obj is FloatEase other && Equals(other);
+        public override int GetHashCode() => _value.GetHashCode();
+        public int CompareTo(FloatEase other) => _value.CompareTo(other._value);
+
+        public override string ToString() => _value.ToString();
+        public string ToString(string format) => _value.ToString(format);
     }
 }
