1a514f4cd8cc388fa2ded47b9f9a2d28d9c659ba
roman.shulevka@pettersonapps.com <micrarriors@gmail.com>
2024-07-19 23:39:26 +0300

    Update ListExtension.cs

--- Assets/DataKeeper/Runtime/Extensions/ListExtension.cs
+++ Assets/DataKeeper/Runtime/Extensions/ListExtension.cs
@@ -1,87 +1,97 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using Random = System.Random;
 
 namespace DataKeeper.Extensions
 {
-   public static class ListExtension
-   {
-      public static void Swap<T>(this List<T> list, int indexA, int indexB)
-      {
-         (list[indexA], list[indexB]) = (list[indexB], list[indexA]);
-      }
-   
-      public static T Pop<T>(this IList<T> list)
-      {
-         if(list.Count == 0)
-            return default;
-      
-         var item = list[0];
-         list.RemoveAt(0);
-         return item;
-      }
-   
-      public static T PopLast<T>(this IList<T> list)
-      {
-         if(list.Count == 0)
-            return default;
-      
-         var item = list[^1];
-         list.Remove(item);
-         return item;
-      }
-   
-      public static T Random<T>(this IList<T> list)
-      {
-         return list[UnityEngine.Random.Range(0, list.Count)];
-      }
-   
-      public static T RandomSystem<T>(this IList<T> list)
-      {
-         Random rnd = new Random();
-         return list[rnd.Next(0, list.Count)];
-      }
-   
-      public static bool HasIndex<T>(this IList<T> list, int index)
-      {
-         if (index < 0) return false;
-         return list.Count > index;
-      }
-   
-      public static T Get<T>(this IList<T> list, int index)
-      {
-         return list.HasIndex(index) ? list[index] : default(T);
-      }
-   
-      public static bool TryGet<T>(this IList<T> list, int index, out T value)
-      {
-         var hasIndex = list.HasIndex(index);
-         value = hasIndex ? list[index] : default(T);
-         return hasIndex;
-      }
-   
-      public static IList<T> Clone<T>(this IList<T> list)
-      {
-         var newList = new List<T>();
-         newList.AddRange(list);
-         return newList;
-      }
+    public static class ListExtension
+    {
+        private static readonly System.Random _systemRandom = new System.Random();
 
-      public static void Shuffle<T>(this List<T> list)
-      {
-         Random rng = new Random();
-         var listB = list.OrderBy(x => rng.Next()).ToList();
-         list.Clear();
-         list.AddRange(listB);
-      }
+        public static void Swap<T>(this List<T> list, int indexA, int indexB)
+        {
+            if (indexA == indexB) return;
+            (list[indexA], list[indexB]) = (list[indexB], list[indexA]);
+        }
 
-      public static T FindRandom<T>(this List<T> list, Predicate<T> match)
-      {
-         var newList = new List<T>();
-         newList.AddRange(list);
-         newList.Shuffle();
-         return newList.Find(match);
-      }
-   }
+        public static T Pop<T>(this List<T> list)
+        {
+            if (list.Count == 0) return default;
+
+            T item = list[0];
+            list.RemoveAt(0);
+            return item;
+        }
+
+        public static T PopLast<T>(this List<T> list)
+        {
+            if (list.Count == 0) return default;
+
+            int lastIndex = list.Count - 1;
+            T item = list[lastIndex];
+            list.RemoveAt(lastIndex);
+            return item;
+        }
+
+        public static T Random<T>(this List<T> list)
+        {
+            return list.Count == 0 ? default : list[UnityEngine.Random.Range(0, list.Count)];
+        }
+
+        public static T RandomSystem<T>(this List<T> list)
+        {
+            return list.Count == 0 ? default : list[_systemRandom.Next(0, list.Count)];
+        }
+
+        public static bool HasIndex<T>(this List<T> list, int index)
+        {
+            return index >= 0 && index < list.Count;
+        }
+
+        public static T Get<T>(this List<T> list, int index)
+        {
+            return list.HasIndex(index) ? list[index] : default;
+        }
+
+        public static bool TryGet<T>(this List<T> list, int index, out T value)
+        {
+            bool hasIndex = list.HasIndex(index);
+            value = hasIndex ? list[index] : default;
+            return hasIndex;
+        }
+
+        public static List<T> Clone<T>(this List<T> list)
+        {
+            return new List<T>(list);
+        }
+
+        public static void Shuffle<T>(this List<T> list)
+        {
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = _systemRandom.Next(n + 1);
+                (list[k], list[n]) = (list[n], list[k]);
+            }
+        }
+
+        public static T FindRandom<T>(this List<T> list, Predicate<T> match)
+        {
+            if (list.Count == 0) return default;
+
+            List<T> matchingItems = list.Where(item => match(item)).ToList();
+            return matchingItems.Count > 0 ? matchingItems[_systemRandom.Next(matchingItems.Count)] : default;
+        }
+
+        public static void RemoveAll<T>(this List<T> list, Predicate<T> match)
+        {
+            list.RemoveAll(match);
+        }
+
+        public static int IndexOf<T>(this List<T> list, Predicate<T> match)
+        {
+            return list.FindIndex(match);
+        }
+    }
 }
